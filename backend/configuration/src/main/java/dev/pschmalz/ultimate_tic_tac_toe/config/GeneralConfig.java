8a40936a7a350a33d924b534995d86918c83d23a
Philipp Schmalz <77fill@googlemail.com>
2024-10-13 15:20:07 +0200

    fix: backend: max idle timeout no longer 30 sec, now MAX

--- backend/configuration/src/main/java/dev/pschmalz/ultimate_tic_tac_toe/config/GeneralConfig.java
+++ backend/configuration/src/main/java/dev/pschmalz/ultimate_tic_tac_toe/config/GeneralConfig.java
@@ -1,12 +1,9 @@
 package dev.pschmalz.ultimate_tic_tac_toe.config;
 
-import java.net.URL;
-import java.util.Objects;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 import org.eclipse.jetty.ee10.servlet.ServletContextHandler;
-import org.eclipse.jetty.ee10.servlet.ServletHolder;
 import org.eclipse.jetty.ee10.websocket.jakarta.server.config.JakartaWebSocketServletContainerInitializer;
 import org.eclipse.jetty.server.Server;
 import org.springframework.context.annotation.Bean;
@@ -31,6 +28,8 @@ public class GeneralConfig {
             // Add echo endpoint to server container
             ServerEndpointConfig echoConfig = ServerEndpointConfig.Builder.create(PlayerEndpoint.class, "/websocket/player").build();
             container.addEndpoint(echoConfig);
+            
+            container.setDefaultMaxSessionIdleTimeout(Long.MAX_VALUE);
         });
 		
 		return jetty;
