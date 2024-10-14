f6bc38d40c36efa621b7b744b1a1ba9bae7b7a6d
Philipp Schmalz <77fill@googlemail.com>
2024-10-14 18:06:13 +0200

    fix: backend: MetaGame init with nulls, null -> ""

--- backend/logic/src/main/java/dev/pschmalz/ultimate_tic_tac_toe/logic/MetaGame.java
+++ backend/logic/src/main/java/dev/pschmalz/ultimate_tic_tac_toe/logic/MetaGame.java
@@ -8,11 +8,17 @@ import dev.pschmalz.ultimate_tic_tac_toe.logic.data.Symbol;
 public class MetaGame {
 	private Symbol[] raw = new Symbol[81];
 	
+	public MetaGame() {
+		for(int i = 0; i < 81; i++) {
+			raw[i] = null;
+		}
+	}
+	
 	public void putSymbol(Symbol symbol, RegularCellCoordinates coords) {
 		System.out.println("Symbol: "+symbol+"\nCoords: "+coords.toString());
 	}
 	
 	public List<String> getListOfStrings() {
-		return Arrays.asList(raw).stream().map(s -> s.toString()).toList();
+		return Arrays.asList(raw).stream().map(s -> s==null?"":s.toString()).toList();
 	}
 }
